5890e829d0cba42a86bc866dc447df8baadcc649
Rodrigo Pupe <rodrigo.pupe@amedigital.com>
2022-12-23 13:17:13 -0300

    Uso de ViewBinding + Utilização da nova extension para definição de cor do texto.

--- app/src/main/java/br/com/heiderlopes/androidmultthemexml/LoyaltyHomeActivity.kt
+++ app/src/main/java/br/com/heiderlopes/androidmultthemexml/LoyaltyHomeActivity.kt
@@ -2,10 +2,17 @@ package br.com.heiderlopes.androidmultthemexml
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import br.com.heiderlopes.androidmultthemexml.databinding.ActivityLoyaltyHomeBinding
 
 class LoyaltyHomeActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoyaltyHomeBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_loyalty_home)
+
+        binding = ActivityLoyaltyHomeBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.tvHello.setTextColor(getColorByThemeAttr(R.attr.primary_700, android.R.color.black))
     }
 }
--- app/src/main/java/br/com/heiderlopes/androidmultthemexml/MainActivity.kt
+++ app/src/main/java/br/com/heiderlopes/androidmultthemexml/MainActivity.kt
@@ -1,57 +1,27 @@
 package br.com.heiderlopes.androidmultthemexml
 
-import android.content.Context
 import android.content.Intent
-import android.content.pm.PackageManager
-import android.content.res.Resources
-import android.content.res.TypedArray
-import android.graphics.Color
 import android.os.Bundle
-import android.util.TypedValue
-import android.widget.Button
-import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-
+import br.com.heiderlopes.androidmultthemexml.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var tvHello: TextView
-    private lateinit var btNext: Button
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        //ThemeManager[this] = R.style.Theme_AndroidMultThemeXML_Loyalty;
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
-        tvHello = findViewById(R.id.tvHello)
-        btNext = findViewById(R.id.btNext)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
-        btNext.setOnClickListener {
+        binding.btNext.setOnClickListener {
             startActivity(
                 Intent(this, LoyaltyHomeActivity::class.java)
             )
         }
 
-        tvHello.setTextColor(getColorByThemeAttr(this, R.attr.primaryTextColor, R.color.colorOnPrimary))
-        //setColor()
-    }
-
-    private fun setColor() {
-        val attrs = intArrayOf(R.attr.primaryTextColor)
-        try { //getPackageManager() can throw an exeption
-            val themeId = this.packageManager.getActivityInfo(this.componentName, 0).theme
-            val ta: TypedArray = this.obtainStyledAttributes(themeId, attrs)
-            val color = ta.getColor(0, Color.BLACK) //I set Black as the default color
-            tvHello.setTextColor(color)
-            ta.recycle()
-        } catch (e: PackageManager.NameNotFoundException) {
-            e.printStackTrace()
-        }
-    }
-
-    private fun getColorByThemeAttr(context: Context, attr: Int, defaultColor: Int): Int {
-        val typedValue = TypedValue()
-        val theme: Resources.Theme = context.theme
-        val got: Boolean = theme.resolveAttribute(attr, typedValue, true)
-        return if (got) typedValue.data else defaultColor
+        binding.tvHello.setTextColor(
+            getColorByThemeAttr(R.attr.primary_700, android.R.color.black)
+        )
     }
 }
